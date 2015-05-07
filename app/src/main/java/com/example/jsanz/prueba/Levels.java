68ee08759ba44a5dcdf482ae7e40550864ff1d89
metalsanz <hellracer666@hotmail.com>
2015-05-07 18:01:02 +0200

    Creadas las pantallas de informacion y la de seleccion de nivel y los botones respectivos para navegar entre ellas. Creados los strings que iran en la pantalla de infromacion. creado el documento de valores de colors.xml para almacenar los colores que iremos utilizando en las sucesivas pantallas de la aplicacion.

--- app/src/main/java/com/example/jsanz/prueba/Levels.java
+++ app/src/main/java/com/example/jsanz/prueba/Levels.java
@@ -1,40 +1,24 @@
 package com.example.jsanz.prueba;
 
-import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
 
 
-public class MainActivity extends Activity {
+public class Levels extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_levels);
     }
 
 
-    public void toInfo(View v){
-        Intent i = new Intent(this,Info.class);
-        startActivity(i);
-    }
-
-    public void toPlay(View v){
-        Intent i =new Intent(this,Levels.class);
-        startActivity(i);
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_levels, menu);
         return true;
     }
 
